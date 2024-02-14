b2a90fd697357c6d7537783b3f1891157811e1c1
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-02-14 09:25:55 -0600

    sm6225-common: rootdir: Dynamically set IRQ affinity for KGSL and MSM_DRM
    - Introduce a new shell script to set IRQ affinity, since different
      kernels can have different IRQ numbers.
    
    Change-Id: I61d2350e6b40f096714148c568489579bc4d2371
    Co-Authored-By: Alex Naidis <alex.naidis@paranoidandroid.co>
    Co-Authored-By: Jake Weinstein <jake@aospa.co>

--- common.mk
+++ common.mk
@@ -316,6 +316,7 @@ PRODUCT_PACKAGES += \
     init.qti.early_init.sh \
     init.qti.kernel.sh \
     init.qti.write.sh \
+    init.sm6225_perf.sh \
     system_dlkm_modprobe.sh \
     vendor_modprobe.sh
 
