e1ae7c3103ea30c24499c959aba208f5f6896803
Bruno Martins <bgcngm@gmail.com>
2024-10-03 10:00:26 +0100

    sm6225-common: Reserve space for gapps
    Change-Id: I8f6c11c3c721c54c008f92fa77287a8c3dcf28fe

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -240,3 +240,6 @@ WIFI_DRIVER_STATE_ON := "ON"
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+
+# Reserve space for gapps installation and other customizations
+-include vendor/lineage/config/BoardConfigReservedSize.mk
