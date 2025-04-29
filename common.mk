e1f69aaf7bc5108629ebdde291c26bcab77425c0
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:37:50 +0100

    sm6225-common: Migrate Lineage Health to soong_config_set
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da
    Signed-off-by: ReStranger <restranger@disroot.org>

--- common.mk
+++ common.mk
@@ -363,6 +363,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+$(call soong_config_set, lineage_health, charging_control_charging_path, /sys/class/qcom-battery/input_suspend)
+$(call soong_config_set, lineage_health, charging_control_charging_enabled, 0)
+$(call soong_config_set, lineage_health, charging_control_charging_disabled, 1)
+$(call soong_config_set, lineage_health, charging_control_supports_bypass, false)
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
