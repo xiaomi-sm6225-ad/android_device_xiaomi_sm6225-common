5ba489377abd058660568ddf23832f613b5b6420
LuK1337 <priv.luk@gmail.com>
2025-08-09 11:09:53 +0200

    sm6225-common: Remove vendor/lineage device framework matrix inclusion
    Signed-off-by: ReStranger <restranger@disroot.org>
    Change-Id: I0b9f527de50ca02082be9b92375559f4893b2688

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -215,8 +215,8 @@ BOARD_AVB_ODM_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm sha256
 
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/configs/vintf/framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    $(COMMON_PATH)/configs/vintf/framework_compatibility_matrix.xml
+
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/framework_manifest.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/manifest.xml
 ifneq ($(TARGET_IS_TABLET),true)
