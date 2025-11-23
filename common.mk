0f41c7e5cc050a9afa80b5df1b587a82bc61cd53
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-24 03:08:29 +0800

    sm6225-common: Update some soong config variables to bool type
    Change-Id: Ice4a8e2cc6d756a743116b46299cf833a0e78c57
    Signed-off-by: ReStranger <restranger@disroot.org>

--- common.mk
+++ common.mk
@@ -366,7 +366,7 @@ PRODUCT_PACKAGES += \
 $(call soong_config_set, lineage_health, charging_control_charging_path, /sys/class/qcom-battery/input_suspend)
 $(call soong_config_set, lineage_health, charging_control_charging_enabled, 0)
 $(call soong_config_set, lineage_health, charging_control_charging_disabled, 1)
-$(call soong_config_set, lineage_health, charging_control_supports_bypass, false)
+$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
 
 # Media
 PRODUCT_PACKAGES += \
