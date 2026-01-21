98ab0c9a119c2c1953efd3efccaa0e3a16cf5522
Bruno Martins <bgcngm@gmail.com>
2026-01-21 22:30:20 +0300

    sm6225-common: Use aidl sensors multihal
    Change-Id: I21da3da39e9726dca693d6bcbe42e93be861f7c7
    Signed-off-by: ReStranger <restranger@disroot.org>

--- common.mk
+++ common.mk
@@ -514,7 +514,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@2.1-service.xiaomi-multihal \
+    android.hardware.sensors-service.multihal \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
