7381b126255216cb807c60768612804de61e1b65
ReStranger <restranger@disroot.org>
2026-01-24 17:46:58 +0300

    [TMP] [HACK]: Set selinux to permissive
    Signed-off-by: ReStranger <restranger@disroot.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -95,7 +95,8 @@ BOARD_MKBOOTIMG_INIT_ARGS += --header_version $(BOARD_INIT_BOOT_HEADER_VERSION)
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
-    androidboot.usbcontroller=4e00000.dwc3
+    androidboot.usbcontroller=4e00000.dwc3 \
+    androidboot.selinux=permissive
 
 BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000
